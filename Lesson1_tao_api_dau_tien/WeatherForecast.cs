c6b46803d22f7e18d678fb4edb0e92b7965fcbf3
Duckun1909 <duc.ph.2045@aptechlearning.edu.vn>
2023-06-21 22:59:50 +0700

    tao api dau tien

---
+++ Lesson1_tao_api_dau_tien/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace Lesson1_tao_api_dau_tien
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
